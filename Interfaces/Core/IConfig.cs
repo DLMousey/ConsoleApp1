c86f2a2cdc954d02aff81343cc81b63c41b3eaa7
Lesley van der Lee <l.lee@tayo.ws>
2019-06-25 18:24:49 +0200

    Update the config interface as well

--- Interfaces/Core/IConfig.cs
+++ Interfaces/Core/IConfig.cs
@@ -1,7 +1,10 @@
+using ConsoleApp1.Interfaces.Core.Database;
+
 namespace ConsoleApp1.Interfaces.Core
 {
     public interface IConfig
     {
+        IDatabase postgresql { get; set; }
         string discordToken { get; set; }
     }
 }
